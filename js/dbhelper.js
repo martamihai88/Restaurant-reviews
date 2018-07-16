9458432567fa4b04de241bef48d31111458f11b9
Mihai Marta <martamihai88@yahoo.com>
2018-07-16 22:33:25 +0300

    change server port

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,11 +9,11 @@ class DBHelper {
    */
   static get DATABASE_URL() {
       
-      const port = 1525 // Change this to your server port
-    return `http://127.0.0.1:${port}/data/restaurants.json`;
+   /*const port = 1525 // Change this to your server port
+    return `http://127.0.0.1:${port}/data/restaurants.json`;*/
       
-   /* const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;*/
+    const port = 8000 // Change this to your server port
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
